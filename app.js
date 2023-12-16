ce15426d7a2e36bcc713f7d5acb602c2cfe0e8f0
Bharathidevi <bharathimogalapu3@gmail.com>
2023-12-16 16:13:59 +0530

    second Commit

--- app.js
+++ app.js
@@ -2,18 +2,57 @@ const express = require("express");
 const app = express();
 const cors=require("cors");
 app.use(cors());
+
 const videos=require("./API1.json");
+const trending=require("./API2.json");
+const gaming=require("./API3.json");
 
 app.listen(3000,()=>{
     console.log("server started");
 })
 
 app.get("/",(req,res)=>{
-    res.send("Welcome");
+    res.send("Welcome to NxtWatch!");
 });
 
-app.get("/videos/all",(req,res)=>{
+app.get("/videos",(req,res)=>{
     res.send(videos);
 });
+
+app.get("/videos/trending",(req,res)=>{
+    res.send(trending);
+});
+
+app.get("/videos/gaming",(req,res)=>{
+    res.send(gaming);
+});
+
+
+app.get("videos/:id",(req,res)=>{
+    let result=[];
+    const {id}=req.params;
+    const video=videos[videos];
+    for(let singleVideo of video)
+    {
+        if(singleVideo[id]===id){
+            result.push(singleVideo);
+        }
+    }
+    res.send(result);
+});
+
+app.get("videos/:title",(req,res)=>{
+    let result=[];
+    const {title}=req.params;
+    const video=videos[videos];
+    for(let singleVideo of video)
+    {
+        if(singleVideo[title]===title){
+            result.push(singleVideo);
+        }
+    }
+    res.send(result);
+});
+
 
 module.exports = app;
