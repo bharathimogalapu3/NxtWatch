81e4aca70b86994e8a5adf1013fca05b2478488c
Bharathidevi <bharathimogalapu3@gmail.com>
2023-12-16 15:17:25 +0530

    second Commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors=require("cors");
-app.use(cors);
+app.use(cors());
 const videos=require("./API1.json");
 
 app.listen(4000,()=>{
